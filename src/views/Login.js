15d2872159cf56d3e521656bcab2518f89913a17
drangonh <786676631@qq.com>
2018-06-06 19:12:29 +0800

    0.1

--- src/views/Login.js
+++ src/views/Login.js
@@ -1,18 +1,33 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import logo from '../logo.svg';
 import '../style/App.css';
+import '../style/view.css';
 
 class Login extends Component {
+
+    login() {
+        alert(55);
+    }
+
     render() {
         return (
-            <div className="App">
-                <header className="App-header">
-                    <img src={logo} className="App-logo" alt="logo" />
+            <div className="App basicBg textCenter">
+                <header className="App-header center">
+                    <img src={logo} className="App-logo" alt="logo"/>
                     <h1 className="App-title">Hello World</h1>
                 </header>
-                <p className="App-intro">
-                    To get started, edit <code>src/App.js</code> and save to reload.
-                </p>
+                <div className="flex udc basicBg center">
+                    <div className="App-intro h50 flex center">
+                        <p>管理员姓名：</p>
+                        <textarea className="h2"/>
+                    </div>
+                    <div className="App-intro flex h50 center">
+                        <p>管理员密码：</p>
+                        <textarea className="h2"/>
+                    </div>
+
+                    <button className="loginButton center App-intro" onClick={() => this.login()}>登陆</button>
+                </div>
             </div>
         );
     }
