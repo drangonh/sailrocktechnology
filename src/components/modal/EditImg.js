8ab2fc785ba0fb23b851bbac9fd89d9001b91ae7
drangonh <786676631@qq.com>
2018-06-14 14:50:12 +0800

    图片上传成功

--- src/components/modal/EditImg.js
+++ src/components/modal/EditImg.js
@@ -1,22 +1,26 @@
-import {Icon, Form, Input, Modal, Upload, message, Select} from 'antd';
+import {Icon, Form, Input, Modal, Upload, message, Select, Button} from 'antd';
 import React, {Component} from 'react';
 import 'antd/dist/antd.css';
 import '../../style/Navi.css'
 import '../../style/view.css'
 import {fetchPost} from "../../client";
-
-const FormItem = Form.Item;
-const baseUrl = "http://www.smarticloudnet.com";
-const imgFile = null;
+import reqwest from 'reqwest';
 
 class EditImg extends Component {
-    state = {
-        loading: false,
-        imageUrl: "",
-        upload: false,
-        formData: new FormData()
-    };
-    name = "";
+    constructor(props) {
+        super(props);
+        this.state = {
+            loading: false,
+            imageUrl: "",
+            upload: false,
+            formData: new FormData(),
+            fileList: []
+        };
+        this.name = "";
+        this.pid = "";
+        this.url = "http://www.smarticloudnet.com/shop/manager/product/update_img?pid=";
+    }
+
 
     /*
      *上传之前判断类型，返回true或者false；或者返回一个promise
@@ -59,62 +63,68 @@ class EditImg extends Component {
     }
 
     /*上传图片并且改变显示*/
-    handleChange = (info) => {
-        if (info.file.status === 'uploading') {
-            this.setState({
-                loading: true
-            });
-            return;
-        }
-        if (info.file.status === 'done') {
-            console.log(this.state.formData)
-            console.log(info);
-            // Get this url from response in real world.
-            this.getBase64(info.file.originFileObj, imageUrl => {
+    handleUpload = () => {
+        const {fileList} = this.state;
+        const formData = new FormData();
+        fileList.forEach((file) => {
+            formData.append('fileName', file);
+        });
+
+        this.setState({
+            uploading: true,
+        });
+
+        reqwest({
+            url: this.url + this.pid,
+            method: 'post',
+            processData: false,
+            data: formData,
+            success: () => {
                 this.setState({
-                    imageUrl,
-                    loading: false,
-                    upload: true
-                }, message.info("上传成功！"))
-            });
-        } else {
-            this.setState({
-                loading: false,
-                upload: true
-            }, message.info("上传失败！"));
-        }
+                    fileList: [],
+                    uploading: false,
+                });
+                message.success('上传成功');
+            },
+            error: () => {
+                this.setState({
+                    uploading: false,
+                });
+                message.error('上传失败');
+            },
+        });
     }
 
 
     render() {
-        const {getFieldDecorator} = this.props.form;
-        const formItemLayout = {
-            labelCol: {
-                xs: {span: 24},
-                sm: {span: 7},
-            },
-            wrapperCol: {
-                xs: {span: 24},
-                sm: {span: 16},
-            },
-        };
-        const tailFormItemLayout = {
-            wrapperCol: {
-                span: 14,
-                offset: 7
-            }
-        };
-
+        this.pid = JSON.stringify(this.props.oneList) != "{}" ? this.props.oneList.pid : "";
         const uploadButton = (
             <div>
                 <Icon type={this.state.loading ? 'loading' : 'plus'}/>
                 <div className="ant-upload-text">Upload</div>
             </div>
         );
-        const imageUrl = this.state.imageUrl;
-        const Option = Select.Option;
-        const pid = JSON.stringify(this.props.oneList) != "{}" ? this.props.oneList.pid : "";
-        const url = "http://www.smarticloudnet.com/shop/manager/product/update_img?pid=" + pid;
+        const {loading} = this.state;
+        const props = {
+            action: this.url + this.pid,
+            onRemove: (file) => {
+                this.setState(({fileList}) => {
+                    const index = fileList.indexOf(file);
+                    const newFileList = fileList.slice();
+                    newFileList.splice(index, 1);
+                    return {
+                        fileList: newFileList,
+                    };
+                });
+            },
+            beforeUpload: (file) => {
+                this.setState(({fileList}) => ({
+                    fileList: [...fileList, file],
+                }));
+                return false;
+            },
+            fileList: this.state.fileList,
+        };
         return (
             <Modal
                 okText={"确认"}
@@ -122,44 +132,26 @@ class EditImg extends Component {
                 title={"编辑图片"}
                 visible={this.props.visible}
                 onCancel={() => {
-                    if (this.state.upload) {
-                        this.props.onCancel()
-                    } else {
-                        message.info("请等待上传！");
-                    }
+                    this.props.onCancel()
                 }}
                 onOk={() => {
-                    if (this.state.upload) {
-                        this.props.onOk()
-                    } else {
-                        message.info("请等待上传！");
-                    }
+                    this.props.onOk()
                 }}
             >
-                <Form onSubmit={this.handleSubmit}>
-                    <FormItem
-                        {...formItemLayout}
-                        label="商品图片"
-                    >
-                        {getFieldDecorator('image', {
-                            rules: [{required: true, message: '请选择商品图片！'}]
-                        })(
-                            <Upload
-                                data={this.state.formData}
-                                name="avatar"
-                                listType="picture-card"
-                                className="avatar-uploader"
-                                showUploadList={false}
-                                action={url}   //上传的地址，必填参数
-                                beforeUpload={(file) => this.beforeUpload(file)}
-                                onChange={(info) => this.handleChange(info)}
-                            >
-                                {imageUrl ? <img style={{width: "50px", height: "50px"}} src={imageUrl}
-                                                 alt="avatar"/> : uploadButton}
-                            </Upload>
-                        )}
-                    </FormItem>
-                </Form>
+                <Upload {...props}>
+                    <Button>
+                        <Icon type="upload"/> Select File
+                    </Button>
+                </Upload>
+                <Button
+                    className="upload-demo-start"
+                    type="primary"
+                    onClick={() => this.handleUpload()}
+                    disabled={false}
+                    loading={loading}
+                >
+                    {loading ? 'Uploading' : 'Start Upload'}
+                </Button>
             </Modal>
         )
     }
